a15b3aaf130adb9086a524e8b7e36cb571a39a60
jaimetam <tamesjaime96@gmail.com>
2024-02-01 21:20:58 -0600

    coordinates of the mouse pointer

--- assets/script.js
+++ assets/script.js
@@ -10,3 +10,14 @@ async function getWeather(latitude, longitude){
     .then((response) => response.json())
     .then((response) => {console.log(response)});
 }
+
+
+map.on('mousemove', (e) => {
+    document.getElementById('info').innerHTML =
+    // `e.point` is the x, y coordinates of the `mousemove` event
+    // relative to the top-left corner of the map.
+    JSON.stringify(e.point) +
+    '<br />' +
+    // `e.lngLat` is the longitude, latitude geographical position of the event.
+    JSON.stringify(e.lngLat.wrap());
+    });

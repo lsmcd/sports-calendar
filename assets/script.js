6d4a241fd2a7002d82d7ea0284fb75b77ccaffea
Logan McDaniel <lsmcd@pm.me>
2024-02-01 21:10:42 -0600

    start of JS weather code

--- assets/script.js
+++ assets/script.js
@@ -1,4 +1,12 @@
-var getSeasons = function(){
-
-
+//More parameters to be added
+async function getWeather(latitude, longitude){
+    var url = `https://api.open-meteo.com/v1/forecast?latitude=` + 
+    latitude + 
+    `&longitude=` + 
+    longitude + 
+    `&hourly=temperature_2m&forecast_days=1`
+    var weather;
+    fetch(url)
+    .then((response) => response.json())
+    .then((response) => {console.log(response)});
 }

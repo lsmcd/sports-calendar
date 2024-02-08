3f2c7386ff57cc65386a16da849b0d221e34acd4
xJohnnyy <152817770+xJohnnyy@users.noreply.github.com>
2024-02-07 20:50:13 -0600

    added to html

--- assets/script.js
+++ assets/script.js
@@ -1,4 +1,33 @@
-var getSeasons = function(){
+mapboxgl.accessToken = 'pk.eyJ1IjoiamFpbWV0YW0iLCJhIjoiY2xzNDBtNDVsMGVlbDJsbzNtMHQ0MWY3ZyJ9.sG8PAbO7cdBBVwT-seUtuA';
+const map = new mapboxgl.Map({
+container: 'map', // container ID
+center: [-74.5, 40], // starting position [lng, lat]
+zoom: 9 // starting zoom
+});
 
 
+//More parameters to be added
+async function getWeather(latitude, longitude){
+    var url = `https://api.open-meteo.com/v1/forecast?latitude=` + 
+    latitude + 
+    `&longitude=` + 
+    longitude + 
+    `&hourly=temperature_2m&forecast_days=1`
+    var weather;
+    fetch(url)
+    .then((response) => response.json())
+    .then((response) => {console.log(response)});
+
+    console.log(latitude)
 }
+
+
+map.on('mousemove', (e) => {
+    document.getElementById('info').innerHTML =
+    // `e.point` is the x, y coordinates of the `mousemove` event
+    // relative to the top-left corner of the map.
+    JSON.stringify(e.point) +
+    '<br />' +
+    // `e.lngLat` is the longitude, latitude geographical position of the event.
+    JSON.stringify(e.lngLat.wrap());
+    });

fa37a14da9d9012439e40dcdb669df3799fd8414
Logan McDaniel <lsmcd@pm.me>
2024-02-08 21:11:14 -0600

    weather code

--- assets/script.js
+++ assets/script.js
@@ -31,13 +31,54 @@ map.on('mousemove', (e) => {
      JSON.stringify(e.lngLat.wrap());
      });
  
- map.on('click',  (e) => {
-     var point = e.lngLat
-     var lat = point.lat
-    var lng = point.lng
- console.log(lat, lng)
- var url = `https://api.open-meteo.com/v1/forecast?latitude=` + lat + `&longitude=` + lng 
- fetch(url)
- .then((response) => response.json())
- .then((response) => {console.log(response)});
- })
+map.on('click',  (e) => {
+    var point = e.lngLat;
+    var lat = point.lat;
+    var lng = point.lng;
+    console.log(lat, lng);
+    getWeather(lat, lng);
+});
+function getWeather(lat, lon){
+    url = `https://api.open-meteo.com/v1/forecast`
+    url += `?latitude=` + lat
+    url += `&longitude=` + lon
+    url += `&current=temperature_2m,relative_humidity_2m,apparent_temperature,weather_code,wind_speed_10m&timeformat=unixtime&timezone=auto`
+    if (localStorage.getItem("imperial")){
+        url += `&temperature_unit=fahrenheit&wind_speed_unit=mph&precipitation_unit=inch`
+    };
+    fetch(url)
+    .then((response) => response.json())
+    .then(function(data){
+        var temperature = data.current.temperature_2m + " " + data.current_units.temperature_2m;
+        var relativeHumidity = data.current.relative_humidity_2m + " " + data.current_units.relative_humidity_2m;
+        var weather = getWeatherByCode(data.current.weather_code);
+        var windSpeed = data.current.wind_speed_10m + " " + data.current_units.wind_speed_10m;
+        var apparentTemperature = data.current.apparent_temperature + " " + data.current_units.apparent_temperature;
+        console.log(temperature, relativeHumidity, weather, windSpeed, apparentTemperature, maxTemperature, minTemperature)
+    });
+}
+function getWeatherByCode(weatherCode){
+    switch(weatherCode){
+        case 0:
+            return "Clear sky";
+            break;
+        case 1: 
+            return "Mainly clear";
+            break;
+        case 2: 
+            return "Partly cloudy";
+            break;
+        case 3:
+            return "Overcast";
+            break;
+        case 45:
+            return "Fog";
+            break;
+        case 48:
+            return "Depositing rime fog";
+            break;
+        case 51:
+            break;
+        // ill do the rest later
+    }
+}
